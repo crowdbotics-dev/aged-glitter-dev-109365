681491d124f887b4c004a12b002370406d784a0c
Crowdbotics <engineering@crowdbotics.com>
2023-08-15 23:17:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-glitter-dev-109365",
+  url: "https://aged-glitter-dev-109365.botics.co",
+  api: "https://aged-glitter-dev-109365.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
